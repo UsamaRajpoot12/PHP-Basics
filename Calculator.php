e2caedf601e295bb56a581dac4e8e8e097c6d6d7
UsamaRajpoot12 <146440809+UsamaRajpoot12@users.noreply.github.com>
2023-09-29 16:38:23 +0500

    Add files via upload

---
+++ Calculator.php
@@ -0,0 +1,23 @@
+<?php 
+
+$x=20;
+$y=10; 
+
+$sum=  $x + $y ;
+   
+    echo "<b>Addition=</b>".$sum ,"<br>";
+
+$sub=  $x - $y;
+
+    echo "<b>Subtraction=</b>".$sub,"<br>";
+
+$multiply=  $x * $y ;
+
+    echo "<b>Multiply=</b>".$multiply,"<br>";
+
+$divison=  $x / $y ;
+
+    echo "<b>Division=</b>".$divison,"<br>";
+
+
+?>
